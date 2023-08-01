36ee19f5ced6e4105fc3612026c005cc8b5e3932
tapsaca <98756983+tapsaca@users.noreply.github.com>
2023-08-01 23:45:52 +0300

    Add data fetching for current price

--- src/screens/CurrentPrice.js
+++ src/screens/CurrentPrice.js
@@ -1,9 +1,21 @@
+import { useEffect, useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
 const CurrentPrice = () => {
+  const [date, setDate] = useState(new Date())
+  const [price, setPrice] = useState([])
+
+  console.log(date)
+
+  useEffect(() => {
+    fetch(`https://api.porssisahko.net/v1/price.json?date=${date.toISOString().split('T')[0]}&hour=${date.getHours()}`)
+      .then((response) => response.json())
+      .then((priceData) => setPrice(priceData['price']))
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text style={styles.priceText}>0.15 € / kWh</Text>
+      <Text style={styles.priceText}>{price} c / kWh</Text>
     </View>
   )
 }
