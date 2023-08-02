209835cdd197d6c091a45d629252c673e3db66fd
tapsaca <98756983+tapsaca@users.noreply.github.com>
2023-08-02 21:02:47 +0300

    Change data fetching to happen every hour

--- src/screens/CurrentPrice.js
+++ src/screens/CurrentPrice.js
@@ -5,13 +5,15 @@ const CurrentPrice = () => {
   const [date, setDate] = useState(new Date())
   const [price, setPrice] = useState([])
 
-  console.log(date)
-
   useEffect(() => {
     fetch(`https://api.porssisahko.net/v1/price.json?date=${date.toISOString().split('T')[0]}&hour=${date.getHours()}`)
       .then((response) => response.json())
       .then((priceData) => setPrice(priceData['price']))
-  }, [])
+    const timer = setTimeout(() => {
+      setDate(new Date())
+    }, 3600000 - new Date().getTime() % 3600000)
+    return () => clearTimeout(timer)
+  }, [date])
 
   return (
     <View style={styles.container}>
